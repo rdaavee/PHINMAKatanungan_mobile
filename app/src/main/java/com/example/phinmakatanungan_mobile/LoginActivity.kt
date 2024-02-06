a6f1946f6aec4696818533e06d05f688da0421d0
ran <rdavee0412@gmail.com>
2024-02-06 13:41:21 +0800

    created WelcomeActivity and added OnClickListener

--- app/src/main/java/com/example/phinmakatanungan_mobile/LoginActivity.kt
+++ app/src/main/java/com/example/phinmakatanungan_mobile/LoginActivity.kt
@@ -1,11 +1,22 @@
 package com.example.phinmakatanungan_mobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        findViewById<TextView>(R.id.tv_signup2).setOnClickListener {
+            startActivity(Intent(this@LoginActivity, SignupActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.btnLogin2).setOnClickListener {
+            startActivity(Intent(this@LoginActivity, DashboardFragment::class.java))
+        }
     }
 }
