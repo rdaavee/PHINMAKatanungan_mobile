7bd0b19f33176aa955ad6acc9c584d0d78a7f822
Johann C <joso.custodio.up@phinmaed.com>
2024-02-10 16:56:36 +0800

    added signup functionality. mobileapi is a pre-requisite so make sure to install that first. repo link was given in the gc.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,11 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -47,4 +52,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //retrofit and gson libraries
+
+    implementation("com.squareup.retrofit2:retrofit:2.5.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.5.0")
 }
