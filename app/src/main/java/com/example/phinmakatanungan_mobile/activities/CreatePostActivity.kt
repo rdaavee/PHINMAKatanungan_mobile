3d6bdd1b41219fbba34a9800a552ed3f04dcafb1
ran <rdavee0412@gmail.com>
2024-02-12 16:19:50 +0800

    added Create Activity with layout

--- app/src/main/java/com/example/phinmakatanungan_mobile/activities/CreatePostActivity.kt
+++ app/src/main/java/com/example/phinmakatanungan_mobile/activities/CreatePostActivity.kt
@@ -1,12 +1,25 @@
 package com.example.phinmakatanungan_mobile.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import com.example.phinmakatanungan_mobile.R
 
 class CreatePostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_post)
+
+        findViewById<ImageView>(R.id.iv_backBtnCreate).setOnClickListener {
+            startActivity(Intent(this@CreatePostActivity, DashboardActivity::class.java))
+        }
+
+        findViewById<TextView>(R.id.tv_createPost).setOnClickListener {
+            startActivity(Intent(this@CreatePostActivity, DashboardActivity::class.java))
+        }
+
+
     }
 }
